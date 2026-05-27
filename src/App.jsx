9dbae7f03671967d99df35529c83d89e576a893e
brahmanyasudulagunta <98081415+brahmanyasudulagunta@users.noreply.github.com>
2026-05-27 11:08:41 -0500

    updated api limit model

--- src/App.jsx
+++ src/App.jsx
@@ -91,7 +91,6 @@ const App = () => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           contents: formattedMessages,
-          tools: [{ google_search: {} }],
           safetySettings: [
             { category: "HARM_CATEGORY_HARASSMENT", threshold: "BLOCK_NONE" },
             { category: "HARM_CATEGORY_HATE_SPEECH", threshold: "BLOCK_NONE" },
